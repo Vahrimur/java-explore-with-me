7f46906e0909b689267fd17defd3fca936caaef3
Vahrimur <vierys@yandex.ru>
2022-11-09 01:05:35 +0300

    fix: убрать неиспользующийся импорт

--- ewm-server/src/main/java/ru/practicum/comment/controller/AdminCommentController.java
+++ ewm-server/src/main/java/ru/practicum/comment/controller/AdminCommentController.java
@@ -1,6 +1,5 @@
 package ru.practicum.comment.controller;
 
-import com.sun.istack.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
